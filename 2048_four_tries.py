a1e93247e14dcf33724f11ecc87711fb135c2abc
Jonny Mills <jonnymills13huhu@gmail.com>
2018-01-24 06:20:39 -0500

    Update 2048_four_tries.py

--- 2048_four_tries.py
+++ 2048_four_tries.py
@@ -1,3 +1,6 @@
+#I wrote this program to allow the computer to automatically play 2048. It tries 4 times play
+#If your computer doesn't selenium or webdriver to run the code, see video tutorial here: https://www.screencast.com/t/oleCu3Oy
+
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
 
@@ -20,36 +23,3 @@ for i in range(1,5):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-for i in range(1,10):
-    htmlElem.send_keys(Keys.UP)
-    htmlElem.send_keys(Keys.DOWN)
-    for k in range(1,20):
-        htmlElem.send_keys(Keys.LEFT)
-        htmlElem.send_keys(Keys.DOWN)
-
-'''
-
-
-#linkElem = browser.find_element_by_link_text('Try again')
-#linkElem.click()
